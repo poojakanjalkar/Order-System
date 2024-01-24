2d8519ab0b833bbfb366c1a02fb47461bd5d6750
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2024-01-24 11:20:42 +0530

    added new navlink

--- my-order-system-app/src/SHARED/components/Navigation/NavLinks.js
+++ my-order-system-app/src/SHARED/components/Navigation/NavLinks.js
@@ -7,17 +7,22 @@ export default function NavLinks() {
     <ul className="nav-links">
       <li>
         <NavLink to="/" exact activeClassName="active">
-          Users
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/authenticate" exact activeClassName="active">
-          Authenticate
+          ALL USERS
         </NavLink>
       </li>
       <li>
         <NavLink to="/u1/products" exact activeClassName="active">
-          My Products
+          MY PRODUCTS
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/places/new" exact activeClassName="active">
+          ADD PRODUCT
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/authenticate" exact activeClassName="active">
+          AUTHENTICATE
         </NavLink>
       </li>
     </ul>
