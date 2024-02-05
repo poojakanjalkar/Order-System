1dbad8635240c6930a00b9681e85a19cc80bfcdf
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2024-02-05 17:16:29 +0530

    implemented useReducer hook to manage state in input component

--- my-order-system-app/src/Products/pages/NewProduct.js
+++ my-order-system-app/src/Products/pages/NewProduct.js
@@ -5,7 +5,12 @@ import Input from "../../SHARED/FORMELEMENTS/Input";
 export default function NewProduct() {
   return (
     <form className="product-form">
-      <Input element="input" type="text" label="Title" />
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        errorText="please enter valid Title"
+      />
     </form>
   );
 }
