afa66ecdf982cafb49c8c3816e5a7ee01536f52d
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2024-02-12 11:41:12 +0530

    added multiple inputs

--- my-order-system-app/src/Products/pages/NewProduct.js
+++ my-order-system-app/src/Products/pages/NewProduct.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useCallback } from "react";
 import "./NewProduct.css";
 import Input from "../../SHARED/FORMELEMENTS/Input";
-import { VALIDATOR_REQUIRE } from "../../SHARED/util/validators";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../SHARED/util/validators";
 
 export default function NewProduct() {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form className="product-form">
       <Input
+        id="title"
         element="input"
         type="text"
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="please enter valid Title"
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="please enter valid description (at least 5 characters)."
+        onInput={descriptionInputHandler}
       />
     </form>
   );
