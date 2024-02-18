db48b47ff43575ef1cefddcbe40cf46b2de33225
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2024-02-18 22:40:46 +0530

    added modal

--- my-order-system-app/src/Products/components/ProductItem.js
+++ my-order-system-app/src/Products/components/ProductItem.js
@@ -1,27 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProductItem.css";
 import Card from "../../SHARED/UIELEMENTS/Card/Card";
 import Button from "../../SHARED/FORMELEMENTS/Button";
+import Modal from "../../SHARED/UIELEMENTS/Modal";
 
 export default function ProductItem(props) {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleDeleteButton = () => {
+    setShowModal(true);
+  };
   console.log("---***---", props.id);
   return (
-    <li className="place-item">
-      <Card className="place-item__content">
-        <div className="place-item__image">
+    <li className="product-item">
+      <Card className="product-item__content">
+        <div className="product-item__image">
           <img src={props.image} alt={props.title} />
         </div>
-        <div className="place-item__info">
+        <div className="product-item__info">
           <h1>{props.title}</h1>
           <h1>{props.description}</h1>
           <h1>{props.price}</h1>
         </div>
-        <div className="place-item__actions">
+        <div className="product-item__actions">
           <Button inverse>Buy Now</Button>
-
           <Button to={`/products/${props.id}`}>Edit</Button>
 
-          <Button danger>Delete</Button>
+          <Modal
+            header="Are you sure?"
+            footerClass="product-item__modal-actions"
+            footer={
+              <React.Fragment>
+                <Button inverse>CANCEL</Button>
+                <Button danger>DELETE</Button>
+              </React.Fragment>
+            }
+          >
+            <p>
+              Do you want to delete this product? Please note that this action
+              cannot be undone.
+            </p>
+          </Modal>
+          <Button danger onClick={handleDeleteButton}>
+            Delete
+          </Button>
         </div>
       </Card>
     </li>
