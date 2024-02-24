e3812d1e6dc6a74df2efcd41f81359672cd47be5
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2024-02-24 11:59:39 +0530

    work done on authentication

--- my-order-system-app/src/Users/pages/Auth.js
+++ my-order-system-app/src/Users/pages/Auth.js
@@ -1,39 +1,61 @@
 import React from "react";
 import Card from "../../SHARED/UIELEMENTS/Card/Card";
 import Input from "../../SHARED/FORMELEMENTS/Input";
-import { VALIDATOR_EMAIL, VALIDATOR_MIN } from "../../SHARED/util/validators";
+import {
+  VALIDATOR_EMAIL,
+  VALIDATOR_MIN,
+  VALIDATOR_MINLENGTH,
+} from "../../SHARED/util/validators";
 import "./Auth.css";
 import Button from "../../SHARED/FORMELEMENTS/Button";
+import useForm from "../../SHARED/hooks/form-hooks";
 
 export default function Auth() {
+  const [formState, inputHandler] = useForm(
+    {
+      email: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+
+  const authSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
   return (
     <Card className="authentication">
       <h2>Login Required</h2>
       <hr />
-      <form>
+      <form onSubmit={authSubmitHandler}>
         <Input
           id="email"
           element="input"
           label="E-mail"
           type="email"
-          value=""
-          valid=""
-          validators={[VALIDATOR_EMAIL]}
+          validators={[VALIDATOR_EMAIL()]}
           errorText="please enter correct email"
-          onInput={() => {}}
+          onInput={inputHandler}
         />
         <Input
           id="password"
           element="input"
           type="password"
           label="Password"
-          value=""
-          valid=""
-          validators={[VALIDATOR_MIN(3)]}
-          errorText="please enter correct password"
-          onInput={() => {}}
+          validators={[VALIDATOR_MINLENGTH(3)]}
+          errorText="please enter correct password at least 3 characters"
+          onInput={inputHandler}
         />
-        <Button>LOGIN</Button>
+        <Button type="submit" disabled={!formState.isValid}>
+          LOGIN
+        </Button>
       </form>
     </Card>
   );
