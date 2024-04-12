f5636af5227fd4ac33d37640822dc7210d4600bb
Pooja Kanjalkar <poojakanjalkar554@gmail.com>
2024-04-12 16:20:00 +0530

    imported authcontext to ensure authenticated user can view restricted pages

--- my-order-system-app/src/SHARED/components/Navigation/NavLinks.js
+++ my-order-system-app/src/SHARED/components/Navigation/NavLinks.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./NavLinks.css";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../FORMELEMENTS/context/auth-context";
 
 export default function NavLinks() {
+  const auth = useContext(AuthContext);
   return (
     <ul className="nav-links">
       <li>
@@ -10,21 +12,27 @@ export default function NavLinks() {
           ALL USERS
         </NavLink>
       </li>
-      <li>
-        <NavLink to="/u1/products" exact activeClassName="active">
-          MY PRODUCTS
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/products/new" exact activeClassName="active">
-          ADD PRODUCT
-        </NavLink>
-      </li>
-      <li>
-        <NavLink to="/authenticate" exact activeClassName="active">
-          AUTHENTICATE
-        </NavLink>
-      </li>
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/u1/products" exact activeClassName="active">
+            MY PRODUCTS
+          </NavLink>
+        </li>
+      )}
+      {auth.isLoggedIn && (
+        <li>
+          <NavLink to="/products/new" exact activeClassName="active">
+            ADD PRODUCT
+          </NavLink>
+        </li>
+      )}
+      {!auth.isLoggedIn && (
+        <li>
+          <NavLink to="/authenticate" exact activeClassName="active">
+            AUTHENTICATE
+          </NavLink>
+        </li>
+      )}
     </ul>
   );
 }
